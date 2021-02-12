9aac83ad8c176ced1fb23dc2b60c8575b991704c
WooYong Kim <wykim970@gmail.com>
2021-02-12 23:34:34 +0900

    전면 장애물 회피 함수 수정

--- lidar_scan.py
+++ lidar_scan.py
@@ -19,31 +19,49 @@ motor_msg = xycar_motor()
 pub = None
 lidar_range = []
 limit = 0
-limit_before = 0
+flag = 0
+start = 0
+end = 0
 
 def callback(data):
 	global lidar_range, motor_msg
 	lidar_range = data.ranges
 
-def scan_lidar():
-    global limit, limit_before, lidar_range
-    limit_before = limit
-    print('---',limit,'---',limit_before)
-    for degree in range(44,134):	# scan degree
-        if lidar_range[degree] <= 0.4:
-            print(lidar_range[degree])
-            limit += 1
-    if limit > limit_before:
-		return False
-    return True
+def avoid_obstacles():
+    global limit, lidar_range, flag, start, end
 
-def drive(angle, delay):
+    for degree in range(40, 190):       # left = 0', right = 180'
+        # Obstacles in front of vehicle -> go left
+        if flag == 0 and 44 < degree < 134 and lidar_range[degree] <= 0.75:
+            flag = 1
+            limit = 1
+            print('Go Left')
+            print(lidar_range[degree], degree)
+            
+        if flag == 1 and 175 < degree < 189 and 0.45 < lidar_range[degree] <= 0.8:
+            flag = 2
+            limit = 2
+            print('Go Right')
+            print(lidar_range[degree], degree)
+        
+        if flag == 2 and 185 < degree < 189 and 0.1 < lidar_range[degree] < 0.3:
+            flag = 3
+            limit = 3
+            start = time.time()
+            print(start)
+            print('Go Straight')
+            print(lidar_range[degree], degree)
+        
+        if flag == 3:
+            end = time.time()
+            if end - start >= 6:
+                limit = 4
+
+def drive(angle, speed):
     global motor_msg, pub
-    for cnt in range(delay):
-        motor_msg.speed = 3
-        motor_msg.angle = angle
-        pub.publish(motor_msg)
-        rospy.sleep(0.1)
+    motor_msg.speed = speed
+    motor_msg.angle = angle
+    pub.publish(motor_msg)
 
 def stop_msg(delay):
 	global motor_msg, pub
@@ -62,11 +80,17 @@ rospy.sleep(3)
 print("---------- lidar scan drive start ----------")
 
 while not rospy.is_shutdown():
-	if scan_lidar():
-		drive(0,10)
-	else:
-		drive(30,10)	# drive right for obstacles
-		
+    avoid_obstacles()
+    if limit == 0:
+        drive(0, 3)
+    if limit == 1:
+        drive(-40, 2)
+    if limit == 2:
+        drive(50, 2)
+    if limit == 3:
+        drive(20, 2)
+    if limit == 4:
+        drive(-7, 2)
 
 
 
